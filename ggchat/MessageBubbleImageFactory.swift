470245cb2bd69d15480e8297ba81d652d982c2dc
Gary Chang <gwh.chang@gmail.com>
2015-11-09 22:59:07 -0800

    bubble factory

--- ggchat/MessageBubbleImageFactory.swift
+++ ggchat/MessageBubbleImageFactory.swift
@@ -9,17 +9,61 @@
 import Foundation
 import UIKit
 
-class MessageBubbleImageFactory {
+class MessageBubbleImageFactory: NSObject {
+   
+    var bubbleImage: UIImage
+    var capInsets: UIEdgeInsets
+    
+    init(bubbleImage: UIImage, capInsets: UIEdgeInsets) {
+        self.bubbleImage = bubbleImage
+        if (UIEdgeInsetsEqualToEdgeInsets(capInsets, UIEdgeInsetsZero)) {
+            self.capInsets = MessageBubbleImageFactory.gg_centerPointEdgeInsetsForImageSize(bubbleImage.size)
+        } else {
+            self.capInsets = capInsets
+        }
+    }
+    
+    convenience init(bubbleImage: UIImage) {
+        self.init(bubbleImage: bubbleImage, capInsets: UIEdgeInsetsZero)
+    }
     
     func outgoingMessagesBubbleImageWithColor(color: UIColor) -> MessageBubbleImage {
-        
+        return self.gg_messagesBubbleImageWithColor(color, flippedForIncoming: false)
     }
     
     func incomingMessagesBubbleImageWithColor(color: UIColor) -> MessageBubbleImage {
-        
+        return self.gg_messagesBubbleImageWithColor(color, flippedForIncoming: true)
+    }
+   
+    class func gg_centerPointEdgeInsetsForImageSize(bubbleImageSize: CGSize) -> UIEdgeInsets {
+        // make image stretchable from center point
+        let center: CGPoint = CGPointMake(bubbleImageSize.width / 2.0, bubbleImageSize.height / 2.0)
+        return UIEdgeInsetsMake(center.y, center.x, center.y, center.x)
     }
     
-    func messagesBubbleImageWithColor(color: UIColor, flippedForIncoming: Bool) {
+    func gg_messagesBubbleImageWithColor(color: UIColor, flippedForIncoming: Bool) -> MessageBubbleImage {
+        var  normalBubble = self.bubbleImage.gg_imageMaskedWithColor(color)
+        var highlightedBubble = self.bubbleImage.gg_imageMaskedWithColor(
+            color.gg_colorByDarkeningColorWithValue(0.12))
         
+        if (flippedForIncoming) {
+            normalBubble = self.gg_horizontallyFlippedImageFromImage(normalBubble)
+            highlightedBubble = self.gg_horizontallyFlippedImageFromImage(highlightedBubble)
+        }
+        
+        normalBubble = self.gg_stretchableImageFromImage(normalBubble, withCapInsets: self.capInsets)
+        highlightedBubble = self.gg_stretchableImageFromImage(highlightedBubble, withCapInsets: self.capInsets)
+        
+        return MessageBubbleImage(image: normalBubble, highlightedImage:highlightedBubble)
+    }
+    
+    func gg_horizontallyFlippedImageFromImage(image: UIImage) -> UIImage {
+        return UIImage(CGImage:image.CGImage!,
+            scale: image.scale,
+            orientation: UIImageOrientation.UpMirrored)
+    }
+    
+    func gg_stretchableImageFromImage(image: UIImage, withCapInsets capInsets: UIEdgeInsets) -> UIImage {
+        return image.resizableImageWithCapInsets(capInsets, resizingMode:UIImageResizingMode.Stretch)
     }
 }
