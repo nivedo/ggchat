1a5360a6c6b5371b5c5f2114439415b8ebcd8f93
Gary Chang <gwh.chang@gmail.com>
2015-11-25 07:58:45 +0800

    chat cell constraints

--- ggchat/ChatTableViewController.swift
+++ ggchat/ChatTableViewController.swift
@@ -18,6 +18,8 @@ class ChatTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.navigationItem.title = "Chats"
     }
 
     override func didReceiveMemoryWarning() {
